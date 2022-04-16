009c606c2f4fb7f5c1c4d4fc68a900fd82710d71
FranciscoJCC <fjccandelario@gmail.com>
2022-04-16 16:48:21 -0500

    componentes asincronos

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <component :is="component"></component>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineAsyncComponent } from "vue";
+
+const HelloWorld = defineAsyncComponent(() =>
+  import("./components/HelloWorld.vue")
+);
 
 export default {
   name: "App",
   components: {
     HelloWorld,
-  },
-  data() {
-    return {
-      component: "HelloWorld",
-    };
   },
 };
 </script>
